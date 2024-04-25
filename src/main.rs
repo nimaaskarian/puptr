f45023a791e3ef07a4a2e19778d338dbca5d8ea8
nimaaskarian <nimaaskarian.work@outlook.com>
2024-04-25 13:07:43 +0330

    Add stdin reading

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+use std::io::{self, Read};
 mod xml;
 use xml::Xml;
 
-fn main() {
-    let list = Xml::try_from("<a><b><d></d></b><c><e></e></c></a>");
+fn main() -> io::Result<()>{
+    let mut handle = io::stdin().lock();
+    let mut input = String::new();
+    handle.read_to_string(&mut input)?;
+
+    let list = Xml::try_from(input.as_str());
     if let Err(err) = list {
         println!("Error! {:?}", err)
     } else {
         print!("{}", list.unwrap().to_string())
     }
+    Ok(())
 }
