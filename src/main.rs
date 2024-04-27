d39c56a2d1ef0c32df675a22c8a5b6794bebc73e
nimaaskarian <nimaaskarian.work@outlook.com>
2024-04-27 17:05:01 +0330

    Add argument reading

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use std::io::{self, Read};
 mod xml;
 use xml::Xml;
+use std::env;
 
 fn main() -> io::Result<()>{
     let mut handle = io::stdin().lock();
     let mut input = String::new();
     handle.read_to_string(&mut input)?;
+    let args: Vec<String> = env::args().collect();
 
     let xml = Xml::try_from(input.as_str());
     if let Err(err) = xml {
         println!("Error! {:?}", err)
     } else if let Ok(xml) = xml{
+        let xml = if args.len() >= 2 {
+            xml.search_query(&args[1])
+        } else {
+            xml
+        };
         print!("{}", xml.to_string());
     }
     Ok(())
